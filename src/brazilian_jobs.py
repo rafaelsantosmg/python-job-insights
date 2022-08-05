2e11b8963c177040335ce2adec7edfce9e20f31a
Rafael Carvalho <rafaelalonsobarbosa@gmail.com>
2022-08-04 22:36:35 -0300

    feat: req11
    implementado o teste da função brazilian_jobs requisito11

--- src/brazilian_jobs.py
+++ src/brazilian_jobs.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
 from . import jobs
+# import jobs
 
 
 @lru_cache
@@ -23,3 +24,6 @@ def read_brazilian_file(path):
         job["type"] = job.pop("tipo")
 
     return dict_jobs
+
+
+print(read_brazilian_file("tests/mocks/brazilians_jobs.csv"))
--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    report = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    for item in report:
+        assert "title" in item
+        assert "salary" in item
+        assert "type" in item
