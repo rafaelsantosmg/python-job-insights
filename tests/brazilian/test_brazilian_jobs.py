7d8d277da4a2bceac2b9853410a9b020f663a414
Rafael Santos <rafaelalonsobarbosa@gmail.com>
2022-09-16 21:02:32 -0300

    chore(Job Insights): Add projeto Trybe de Python

--- tests/brazilian/test_brazilian_jobs.py
+++
@@ -1,9 +0,0 @@
-from src.brazilian_jobs import read_brazilian_file
-
-
-def test_brazilian_jobs():
-    report = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
-    for item in report:
-        assert "title" in item
-        assert "salary" in item
-        assert "type" in item
--- tests/counter/test_counter.py
+++
@@ -1,6 +0,0 @@
-from src.counter import count_ocurrences
-
-
-def test_counter():
-    assert count_ocurrences("src/jobs.csv", "Python") == 1639
-    assert count_ocurrences("src/jobs.csv", "Javascript") == 122
